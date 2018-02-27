d32f0b955839d20b35afc8dcf8d0b7c7a505bb9a
Noah Finnerman <noahfinnerman@gmail.com>
2018-02-27 03:54:06 -0500

    Add register page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+
   protected
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(:user) || root_path
+  end
+
+  private
+
+  def after_sign_out_path_for(resource)
+    stored_location_for(:user) || root_path
+  end
+
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,13 @@
 class VisitorsController < ApplicationController
+
+
+
+  def index
+    if signed_in?.blank?
+
+    else
+      direct_to register_path_path
+    end
+  end
 end
+
